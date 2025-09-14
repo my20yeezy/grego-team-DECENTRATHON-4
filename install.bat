e13699cd423421929f33032107cd374ee2fb6795
Ernie <yerniyaz.my20@gmail.com>
2025-09-14 23:35:14 +0500

    Deleting old

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Установка зависимостей GREGO OCR...
-python -m venv .venv
-call .venv\.Scripts\activate.bat
-pip install -r requirements.txt
-echo Зависимости установлены! Активируй виртуальное окружение: .venv\Scripts\activate.bat
-pause
--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call .venv\.Scripts\activate.bat
-streamlit run main.py
-pause
